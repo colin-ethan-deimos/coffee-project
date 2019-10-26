95eb7f08b1267686ef40ae6e178c7da12614afe1
colinmarchbanks <marchbanks.colin@gmail.com>
2019-10-25 23:18:21 -0500

    functionality complete

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict"
 
+var storageArr = [];
+
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light',description: "A light variant of City"},
@@ -17,18 +19,26 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark',description: "Ciao"},
     {id: 14, name: 'French', roast: 'dark',description: "Goes great with a cigarette"}
 ];
+const orginalCoffeeLength = coffees.length;
+var retrievedObject = JSON.parse(localStorage.getItem("newCoffee"));
+
+if(retrievedObject !== null){
+    coffees = coffees.concat(retrievedObject);
+}
 
 var cardArea = document.getElementById("coffee-card-area");
 
 var buttonClicked;
+
 var roastButton;
 
 var searchTerm = document.getElementById('searchTerm');
 
 function addCoffee(){
-    for(let i = 0;i<document.getElementsByClassName('roastSelector').length;i++){
-        if(document.getElementsByClassName('roastSelector')[i] === document.getElementsByClassName('roastSelector')[i]).
-            roastButton = document.getElementsByClassName('roastSelector')[i].value;
+    var retrievedObject = JSON.parse(localStorage.getItem("newCoffee"));
+
+    for(let i = 0;i<document.getElementById('roastSelect').length;i++){
+            roastButton = document.getElementById('roastSelect').value;
     }
     let coffee = {
         id: coffees.length + 1,
@@ -37,20 +47,66 @@ function addCoffee(){
         description: document.getElementById("roastDescription").value
     };
     coffees.push(coffee);
+    document.getElementById("enteredName").value = '';
+    document.getElementById('roastSelect').value = '';
+    document.getElementById("roastDescription").value = '';
+    cardArea.innerHTML = '';
+    for(let x = 0; x<coffees.length;x++){
+        cardArea.innerHTML += '<div class="card float-left mx-3 mb-2  border-dark-shade" style="width: 40%;">\n' +
+            '                    <div class="card-body bg-soft-white spookyText">\n' +
+            '                        <h4 class="card-title text-center ">' + coffees[x].name + '</h4>\n' +
+            '                        <h6 class="card-subtitle mb-2 text-muted text-center">' + coffees[x].roast + '</h6>\n' +
+            '                        <p class="card-text text-center">'+ coffees[x].description +'</p>\n' +
+            '                        <button type="button" class="btn btn-outline-primary mx-auto w-100">Buy Now!</button>\n' +
+            '                    </div>\n' +
+            '                </div>'
+    }
+
+    if(retrievedObject === null){
+        storageArr = [];
+        storageArr.push(coffee);
+        localStorage.setItem("newCoffee", JSON.stringify(storageArr));
+    }
+    else{
+        storageArr = JSON.parse(localStorage.getItem("newCoffee"));
+        storageArr.push(coffee);
+        localStorage.setItem("newCoffee", JSON.stringify(storageArr));
+    }
+
 }
 
-
-
-for(let x = 0; x<coffees.length;x++){
-    cardArea.innerHTML += '<div class="card float-left mx-3 mb-2  border-dark-shade" style="width: 40%;">\n' +
-    '                    <div class="card-body bg-soft-white ">\n' +
-    '                        <h5 class="card-title text-center ">' + coffees[x].name + '</h5>\n' +
-    '                        <h6 class="card-subtitle mb-2 text-muted text-center">' + coffees[x].roast + '</h6>\n' +
-    '                        <p class="card-text text-center">'+ coffees[x].description +'</p>\n' +
-    '                        <button type="button" class="btn btn-outline-primary mx-auto w-100">Buy Now!</button>\n' +
-    '                    </div>\n' +
-    '                </div>'
+function clearCoffees(){
+    storageArr = [];
+    localStorage.clear();
+    cardArea.innerHTML = "";
+    for(let x = 0; x<orginalCoffeeLength;x++){
+        cardArea.innerHTML += '<div class="card float-left mx-3 mb-2  border-dark-shade" style="width: 40%;">\n' +
+            '                    <div class="card-body bg-soft-white ">\n' +
+            '                        <h4 class="card-title text-center spookyText">' + coffees[x].name + '</h4>\n' +
+            '                        <h6 class="card-subtitle mb-2 text-muted text-center">' + coffees[x].roast + '</h6>\n' +
+            '                        <p class="card-text text-center">'+ coffees[x].description +'</p>\n' +
+            '                        <button type="button" class="btn btn-outline-primary mx-auto w-100">Buy Now!</button>\n' +
+            '                    </div>\n' +
+            '                </div>'
+    }
 }
+
+function populateCoffeeArea(){
+    cardArea.innerHTML = '';
+    for(let x = 0; x<coffees.length;x++){
+        cardArea.innerHTML += '<div class="card float-left mx-3 mb-2  border-dark-shade" style="width: 40%;">\n' +
+        '                    <div class="card-body bg-soft-white ">\n' +
+        '                        <h4 class="card-title text-center spookyText">' + coffees[x].name + '</h4>\n' +
+        '                        <h6 class="card-subtitle mb-2 text-muted text-center">' + coffees[x].roast + '</h6>\n' +
+        '                        <p class="card-text text-center">'+ coffees[x].description +'</p>\n' +
+        '                        <button type="button" class="btn btn-outline-primary mx-auto w-100">Buy Now!</button>\n' +
+        '                    </div>\n' +
+        '                </div>'
+    }
+}
+
+populateCoffeeArea();
+
 
 function specifyRoast(buttonClicked){
     cardArea.innerHTML = '';
@@ -58,7 +114,7 @@ function specifyRoast(buttonClicked){
         if(buttonClicked === coffees[x].roast){
             cardArea.innerHTML += '<div class="card float-left mx-3 mb-2  border-dark-shade" style="width: 40%;">\n' +
                 '                    <div class="card-body bg-soft-white ">\n' +
-                '                        <h5 class="card-title text-center ">' + coffees[x].name + '</h5>\n' +
+                '                        <h4 class="card-title text-center spookyText ">' + coffees[x].name + '</h4>\n' +
                 '                        <h6 class="card-subtitle mb-2 text-muted text-center">' + coffees[x].roast + '</h6>\n' +
                 '                        <p class="card-text text-center">'+ coffees[x].description +'</p>\n' +
                 '                        <button type="button" class="btn btn-outline-primary mx-auto w-100">Buy Now!</button>\n' +
@@ -67,8 +123,8 @@ function specifyRoast(buttonClicked){
         }
         else if(buttonClicked === 'all'){
             cardArea.innerHTML += '<div class="card float-left mx-3 mb-2  border-dark-shade" style="width: 40%;">\n' +
-                '                    <div class="card-body bg-soft-white ">\n' +
-                '                        <h5 class="card-title text-center ">' + coffees[x].name + '</h5>\n' +
+                '                    <div class="card-body bg-soft-white spookyText">\n' +
+                '                        <h4 class="card-title text-center ">' + coffees[x].name + '</h4>\n' +
                 '                        <h6 class="card-subtitle mb-2 text-muted text-center">' + coffees[x].roast + '</h6>\n' +
                 '                        <p class="card-text text-center">'+ coffees[x].description +'</p>\n' +
                 '                        <button type="button" class="btn btn-outline-primary mx-auto w-100">Buy Now!</button>\n' +
@@ -76,15 +132,11 @@ function specifyRoast(buttonClicked){
                 '                </div>'
         }
     }
-
 }
-
-
 
 for(let i = 0;i<document.getElementsByClassName('roast').length;i++){
     buttonClicked = document.getElementsByClassName('roast').value;
 }
-
 
 searchTerm.addEventListener("input",function () {
     cardArea.innerHTML = '';
@@ -92,7 +144,7 @@ searchTerm.addEventListener("input",function () {
         if(coffees[i].name.toLowerCase().startsWith(searchTerm.value.toLowerCase())){
             cardArea.innerHTML += '<div class="card float-left mx-3 mb-2  border-dark-shade" style="width: 40%;">\n' +
                 '                    <div class="card-body bg-soft-white ">\n' +
-                '                        <h5 class="card-title text-center ">' + coffees[i].name + '</h5>\n' +
+                '                        <h4 class="card-title text-center spookyText ">' + coffees[i].name + '</h4>\n' +
                 '                        <h6 class="card-subtitle mb-2 text-muted text-center">' + coffees[i].roast + '</h6>\n' +
                 '                        <p class="card-text text-center">'+ coffees[i].description +'</p>\n' +
                 '                        <button type="button" class="btn btn-outline-primary mx-auto w-100">Buy Now!</button>\n' +
@@ -102,13 +154,24 @@ searchTerm.addEventListener("input",function () {
     }
 });
 
-console.log(searchTerm);
+window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+        document.getElementById('submitCoffee').addEventListener('click', function(event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+            if(form.checkValidity() === true){
+                addCoffee();
+            }}, false);
+    });
+}   , false);
 
-// var tbody = document.querySelector('#coffees');
-// var submitButton = document.querySelector('#submit');
-// var roastSelection = document.querySelector('#roast-selection');
-//
-// tbody.innerHTML = renderCoffees(coffees);
-//
-// submitButton.addEventListener('click', updateCoffees);
+
+
+
 
