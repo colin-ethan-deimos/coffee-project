3262b838704c1d2e69586f255b441be388c59784
colinmarchbanks <marchbanks.colin@gmail.com>
2019-10-26 17:59:41 -0500

    Added comments to the main.js file

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 "use strict"
-
+// This arr is used as a way to add the created coffees to local storage later on.
 var storageArr = [];
 
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+// Array of coffee objects from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light',description: "A light variant of City"},
     {id: 2, name: 'Half City', roast: 'light' ,description: "A half-strength variant of City"},
@@ -19,28 +19,39 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark',description: "Ciao"},
     {id: 14, name: 'French', roast: 'dark',description: "Goes great with a cigarette"}
 ];
+
+// This const is equal to the original length of the coffee array, used later for repopulating the coffee card area without any added coffees after the local storage is cleared
 const orginalCoffeeLength = coffees.length;
 
+// retrievedObject pulls the coffees from local storage
 var retrievedObject = JSON.parse(localStorage.getItem("newCoffee"));
 
+// cardArea is assigned to be the coffee card area in html
 var cardArea = document.getElementById("coffee-card-area");
 
+// buttonClicked is later used to determine which type of roast is selected with the roast dropdown menu, needs to be a global variable because it is used in a method and a separate function
 var buttonClicked;
 
+// roastButton is similar to the buttonClicked, except this one is used for the addCoffee function, so it must be a separate variable
 var roastButton;
 
+// searchTerm is assigned to be the input of the "Search by Name" card
 var searchTerm = document.getElementById('searchTerm');
 
+// Here if the local storage isn't empty the coffees array is combined with the local storage coffees
 if(retrievedObject !== null){
     coffees = coffees.concat(retrievedObject);
 }
 
+// This fills the coffee card area on load
 populateCoffeeArea();
 
+// This will determine which of the 4 buttons in the select roast dropdown menu are selected and assigns the selected value to the buttonClicked variable
 for(let i = 0;i<document.getElementsByClassName('roast').length;i++){
     buttonClicked = document.getElementsByClassName('roast').value;
 }
 
+// This method is used to populate the coffee card area with coffees that include the input in the Search by Name area
 searchTerm.addEventListener("input",function () {
     cardArea.innerHTML = '';
     for(let i = 0; i<coffees.length;i++){
@@ -57,6 +68,7 @@ searchTerm.addEventListener("input",function () {
     }
 });
 
+// This method I pulled from the bootstrap input documentation, it requires that the input fields in the add coffee areas are valid before allowing the coffee to be added
 window.addEventListener('load', function() {
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.getElementsByClassName('needs-validation');
@@ -74,7 +86,7 @@ window.addEventListener('load', function() {
     });
 }   , false);
 
-
+// This function filters the coffees by the roast type selected in the dropdown menu
 function specifyRoast(buttonClicked){
     cardArea.innerHTML = '';
     for(let x = 0; x<coffees.length;x++){
@@ -101,6 +113,7 @@ function specifyRoast(buttonClicked){
     }
 }
 
+// This function fills the card area with all the coffees in the coffee array without discrimination
 function populateCoffeeArea(){
     cardArea.innerHTML = '';
     for(let x = 0; x<coffees.length;x++){
@@ -115,6 +128,7 @@ function populateCoffeeArea(){
     }
 }
 
+// This function is used to clear the local storage and the reset the coffee card area with only the original coffees
 function clearCoffees(){
     storageArr = [];
     localStorage.clear();
@@ -131,6 +145,7 @@ function clearCoffees(){
     }
 }
 
+// This function is used to add a coffee to the coffee array and to also add it to the local storage at the same time
 function addCoffee(){
     var retrievedObject = JSON.parse(localStorage.getItem("newCoffee"));
     for(let i = 0;i<document.getElementById('roastSelect').length;i++){
